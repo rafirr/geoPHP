ef56260f898894f5c9bfa978ea015b24374b7aad
Rafal Rybarczyk <rybazone@gmail.com>
2019-05-24 11:56:48 +0200

    Multipolygon - return area_id

--- lib/geometry/MultiPolygon.class.php
+++ lib/geometry/MultiPolygon.class.php
@@ -6,11 +6,23 @@ class MultiPolygon extends Collection
 {
   protected $geom_type = 'MultiPolygon';
   
-  public function pointInPolygon($point,$c = false){
+  public function pointInPolygon($point,$c = false, $returnArea = false){
+
+    $area_id = null;
+
     foreach($this->components as $component){
       $c = $component->pointInPolygon($point,$c);
+
+      if($c && !$area_id){
+        $area_id = $component->getSRID();
+      }
     }
-    return $c;
+
+    if(!$returnArea){
+      return $c;
+    }
+    else{
+      return (object) array('status' => $c, 'area_id' => $area_id);
+    }
   }
-  
 }
